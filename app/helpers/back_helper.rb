c914efbb7d4cc2318e42bd934bb7bb5216cce841
Andrew Batallones <batallonesa@live.com>
2017-03-15 19:14:51 -0700

    setting up erb stuff

--- app/helpers/back_helper.rb
+++ app/helpers/back_helper.rb
@@ -1,2 +1,158 @@
 module BackHelper
+  def computer_types
+    [
+      "Desktops",
+      "iMac Aluminum",
+      "iMac Unibody",
+      "iMac Slim",
+      "iMac White",
+      "Mac Mini",
+      "Mac Mini Unibody",
+      "Mac Pro",
+      "Mac Pro Cylinder",
+      "Laptops",
+      "MacBook Pro",
+      "MacBook Pro Unibody",
+      "MacBook Pro Retina",
+      "MacBook White",
+      "MacBook Black",
+      "MacBook White Unibody",
+      "MacBook Alum Unibody",
+      "MacBook Retina (USB-C)",
+      "Macbook Pro Touch Bar",
+      "MacBook Air (1 x USB)",
+      "MacBook Air Unibody"
+    ]
+  end
+
+  def year_section
+    [
+      "Early",
+      "Mid",
+      "Late"
+    ]
+  end
+
+  def year
+    [
+      2018,
+      2017,
+      2016,
+      2015,
+      2014,
+      2013,
+      2012,
+      2011,
+      2010,
+      2009,
+      2008,
+      "other: _________"
+    ]
+  end
+
+  def processor_speed
+    [
+      "1.1 Ghz",
+      "1.2 Ghz",
+      "1.3 Ghz",
+      "1.4 Ghz",
+      "1.6 Ghz",
+      "1.7 Ghz",
+      "1.8 Ghz",
+      "1.83 Ghz",
+      "1.86 Ghz",
+      "2.0 Ghz",
+      "2.13 Ghz",
+      "2.16 Ghz",
+      "2.2 Ghz",
+      "2.26 Ghz",
+      "2.3 Ghz",
+      "2.33 Ghz",
+      "2.4 Ghz",
+      "2.5 Ghz",
+      "2.53 Ghz",
+      "2.6 Ghz",
+      "2.66 Ghz",
+      "2.7 Ghz",
+      "2.8 Ghz",
+      "2.9 Ghz",
+      "2.93 Ghz",
+      "3.0 Ghz",
+      "3.06 Ghz",
+      "3.1 Ghz",
+      "3.2 Ghz",
+      "3.3 Ghz",
+      "3.33 Ghz",
+      "3.4 Ghz",
+      "3.5 Ghz",
+      "3.6 Ghz",
+      "3.7 Ghz",
+      "4.0 Ghz"
+    ]
+  end
+
+  def cpu_type
+    [
+      "Core 2 Duo",
+      "4-Core Xeon (4x Cores)",
+      "6-Core Xeon (6x Cores)",
+      "8-Core Xeon (8x Cores)",
+      "12-Core Xeon (12x Cores)",
+      "Core i3 (Dual Core)",
+      "Core i5 (Dual Core)",
+      "Core i7 (Dual Core)",
+      "Core i5 Quad",
+      "Core i7 Quad",
+      "Core M"
+    ]
+  end
+
+  def screen_size
+    [
+      "N/A",
+      "11 in.",
+      "12 in.",
+      "13 in.",
+      "15 in.",
+      "17 in.",
+      "20 in.",
+      "21.5 in.",
+      "24 in.",
+      "27 in.",
+      "other: _________________"
+    ]
+  end
+
+  def hard_drive
+    [
+      "120 GB",
+      "160 GB",
+      "250 GB",
+      "320 GB",
+      "500 GB",
+      "640 GB",
+      "750 GB",
+      "1 TB",
+      "2 TB",
+      "3 TB",
+      "4 TB",
+      "1 TB Fusion",
+      "4 TB Fusion",
+      "128 GB SSD",
+      "250 GB SSD",
+      "512 GB SSD",
+      "1 TB SSD",
+      "120 GB SSD",
+      "240 GB SSD",
+      "480 GB SSD",
+      "960 GB SSD"
+    ]
+  end
+
+  def yes_no
+    [
+      "yes",
+      "no"
+    ]
+  end
 end
