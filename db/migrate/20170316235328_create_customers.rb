3d3a97e4bac6e570ed3494f57aacf7c03efceb1b
Andrew <andrew@lacomputercompany.com>
2017-03-16 17:18:55 -0700

    Create customer table

--- db/migrate/20170316235328_create_customers.rb
+++ db/migrate/20170316235328_create_customers.rb
@@ -1,8 +1,17 @@
 class CreateCustomers < ActiveRecord::Migration[5.0]
-  def change
+  def up
     create_table :customers do |t|
+      t.string :customer_number
+      t.string :last_name
+      t.string :first_name
+      t.string :email
+      t.string :referred_by
 
       t.timestamps
     end
+  end
+
+  def down
+    drop_table :customers
   end
 end
