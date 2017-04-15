1ff8ef7f751b722adff2dc6f4ecef961af7f1416
Andrew <andrew@lacomputercompany.com>
2017-04-14 17:14:20 -0700

    creating admin to edit sheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  # root 'home#home'
   root 'back#index'
 
-  # get 'back/index'
   get 'back/computer_trade_in'
   get 'back/display'
   get 'back/computer'
   get 'back/onsite'
+
+  # Edit forms
+  get '/adminlogin', to: 'admin#login'
+  get '/adminarea', to: 'admin#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
