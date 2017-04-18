fe8f60036ac74448f9acc749cd6c6a0ed7d2c747
Andrew Batallones <batallonesa@live.com>
2017-04-18 16:39:30 -0700

    more backend stuff

---
+++ app/assets/javascripts/components/computers.js.jsx
@@ -0,0 +1,18 @@
+class Computers extends React.Component {
+    render() {
+        return(
+            <div className='pad'>
+                <a className='btn btn-secondary' href="/adminarea">{ "<< Back" }</a>
+                <h1>Edit Pre-Owned Input Tags</h1>
+                <div className='rows'>
+                    <div className='cols-2'>
+                        <p>Implement Nav system</p>
+                    </div>
+                    <div className='cols-10'>
+                        
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
