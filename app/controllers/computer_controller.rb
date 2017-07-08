850012141db86805cf6c132f33976decb09c97fc
Andrew Batallones <batallonesa@live.com>
2017-07-08 14:23:14 -0700

    added sorting to UsedComputers

--- app/controllers/computer_controller.rb
+++ app/controllers/computer_controller.rb
@@ -3,43 +3,43 @@ class ComputerController < ApplicationController
   before_action :confirm_logged_in, except: [:front_form]
 
   def front_form
-    @desktop_values = parse_values(UsedComputer.search_subcategory("desktop"))
-    @laptop_values = parse_values(UsedComputer.search_subcategory("laptop"))
-    @processor_speed = parse_values(UsedComputer.search_category("processor_speed"))
-    @cpu_type = parse_values(UsedComputer.search_category("cpu_type"))
-    @screen_size = parse_values(UsedComputer.search_category("screen_size"))
-    @hdd = parse_values(UsedComputer.search_subcategory("hdd"))
-    @fusion = parse_values(UsedComputer.search_subcategory("fusion"))
-    @ssd = parse_values(UsedComputer.search_subcategory("ssd"))
-    @optical_drive = parse_values(UsedComputer.search_category("optical_drive"))
-    @card_slot = parse_values(UsedComputer.search_category("card_slot"))
-    @ram = parse_values(UsedComputer.search_category("installed_ram"))
-    @max_ram = parse_values(UsedComputer.search_category("max_ram"))
-    @open_slots = parse_values(UsedComputer.search_category("open_slots"))
-    @os = parse_values(UsedComputer.search_category("os"))
-    @max_os = parse_values(UsedComputer.search_subcategory("max_os"))
-    @nvidia = parse_values(UsedComputer.search_subcategory("nvidia"))
-    @amd = parse_values(UsedComputer.search_subcategory("amd"))
-    @intel = parse_values(UsedComputer.search_subcategory("intel"))
-    @video_ram = parse_values(UsedComputer.search_category("video_ram"))
-    @video_port = parse_values(UsedComputer.search_category("video_port"))
+    @desktop_values = parse_values(UsedComputer.search_subcategory("desktop").sort_by_sub_index)
+    @laptop_values = parse_values(UsedComputer.search_subcategory("laptop").sort_by_sub_index)
+    @processor_speed = parse_values(UsedComputer.search_category("processor_speed").sort_by_sub_index)
+    @cpu_type = parse_values(UsedComputer.search_category("cpu_type").sort_by_sub_index)
+    @screen_size = parse_values(UsedComputer.search_category("screen_size").sort_by_sub_index)
+    @hdd = parse_values(UsedComputer.search_subcategory("hdd").sort_by_sub_index)
+    @fusion = parse_values(UsedComputer.search_subcategory("fusion").sort_by_sub_index)
+    @ssd = parse_values(UsedComputer.search_subcategory("ssd").sort_by_sub_index)
+    @optical_drive = parse_values(UsedComputer.search_category("optical_drive").sort_by_sub_index)
+    @card_slot = parse_values(UsedComputer.search_category("card_slot").sort_by_sub_index)
+    @ram = parse_values(UsedComputer.search_category("installed_ram").sort_by_sub_index)
+    @max_ram = parse_values(UsedComputer.search_category("max_ram").sort_by_sub_index)
+    @open_slots = parse_values(UsedComputer.search_category("open_slots").sort_by_sub_index)
+    @os = parse_values(UsedComputer.search_category("os").sort_by_sub_index)
+    @max_os = parse_values(UsedComputer.search_subcategory("max_os").sort_by_sub_index)
+    @nvidia = parse_values(UsedComputer.search_subcategory("nvidia").sort_by_sub_index)
+    @amd = parse_values(UsedComputer.search_subcategory("amd").sort_by_sub_index)
+    @intel = parse_values(UsedComputer.search_subcategory("intel").sort_by_sub_index)
+    @video_ram = parse_values(UsedComputer.search_category("video_ram").sort_by_sub_index)
+    @video_port = parse_values(UsedComputer.search_category("video_port").sort_by_sub_index)
   end
 
   def index
-    @computer_type = UsedComputer.search_category("computer_type")
-    @processor_speed = UsedComputer.search_category("processor_speed")
-    @cpu_type = UsedComputer.search_category("cpu_type")
-    @screen_size = UsedComputer.search_category("screen_size")
-    @hard_drive = UsedComputer.search_category("hard_drive")
-    @optical_drive = UsedComputer.search_category("optical_drive")
-    @card_slot = UsedComputer.search_category("card_slot")
-    @installed_ram = UsedComputer.search_category("installed_ram")
-    @max_ram = UsedComputer.search_category("max_ram")
-    @open_slots = UsedComputer.search_category("open_slots")
-    @os = UsedComputer.search_category("os")
-    @video_card = UsedComputer.search_category("video_card")
-    @video_ram = UsedComputer.search_category("video_ram")
-    @video_port = UsedComputer.search_category("video_port")
+    @computer_type = UsedComputer.search_category("computer_type").sort_by_sub_index
+    @processor_speed = UsedComputer.search_category("processor_speed").sort_by_sub_index
+    @cpu_type = UsedComputer.search_category("cpu_type").sort_by_sub_index
+    @screen_size = UsedComputer.search_category("screen_size").sort_by_sub_index
+    @hard_drive = UsedComputer.search_category("hard_drive").sort_by_sub_index
+    @optical_drive = UsedComputer.search_category("optical_drive").sort_by_sub_index
+    @card_slot = UsedComputer.search_category("card_slot").sort_by_sub_index
+    @installed_ram = UsedComputer.search_category("installed_ram").sort_by_sub_index
+    @max_ram = UsedComputer.search_category("max_ram").sort_by_sub_index
+    @open_slots = UsedComputer.search_category("open_slots").sort_by_sub_index
+    @os = UsedComputer.search_category("os").sort_by_sub_index
+    @video_card = UsedComputer.search_category("video_card").sort_by_sub_index
+    @video_ram = UsedComputer.search_category("video_ram").sort_by_sub_index
+    @video_port = UsedComputer.search_category("video_port").sort_by_sub_index
   end
 
   def new
@@ -71,6 +71,6 @@ class ComputerController < ApplicationController
   private
 
   def used_computer_params
-    params.require(:used_computer).permit(:category, :sub_category, :value)
+    params.require(:used_computer).permit(:category, :sub_category, :value, :sub_index)
   end
 end
