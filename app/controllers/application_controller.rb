4e900afc98d9c1e3f763578f94c30a2e9494d66c
Andrew Batallones <batallonesa@live.com>
2017-10-05 11:08:15 -0700

    refactor display sheets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  private
+
   def parse_values(data_hash)
     parse_array = []
 
     data_hash.each do |data|
       parse_array.push(data.value)
     end
-    
+
     parse_array
   end
-
-  private
 
   def confirm_logged_in
     unless session[:user_id]
